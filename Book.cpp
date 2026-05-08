3ecbb28274b56f2faa2d1247fa91375106791bbf
FrankV07 <franklynv2125@gmail.com>
2026-05-07 21:10:12 -0400

    Update Book.cpp

--- Book.cpp
+++ Book.cpp
@@ -1,5 +1,6 @@
 #include "Book.h"
 
+// sets up the book with its info makes it not borrowed from the start
 Book::Book(string t, string a, string c) {
     title = t;
     author = a;
@@ -7,7 +8,7 @@ Book::Book(string t, string a, string c) {
     isBorrowed = false;
     dueDate = 0;
 }
-
+// prints book info, shows due date if its currently borrowed
 void Book::display() {
     cout << "Book " << title
          << ", Author:" << author
@@ -17,7 +18,7 @@ void Book::display() {
         cout << "Due Date: " << ctime(&dueDate);
     }
 }
-
+// marks book as borrowed and sets the due date
 void Book::borrowBook(int loanDays) {
     if (!isBorrowed) {
         isBorrowed = true;
@@ -28,7 +29,7 @@ void Book::borrowBook(int loanDays) {
         cout << "Book is already borrowed." << endl;
     }
 }
-
+// marks book as returned and clears the due date
 void Book::returnBook() {
     if (isBorrowed) {
         isBorrowed = false;
@@ -38,7 +39,7 @@ void Book::returnBook() {
         cout << "Book was not borrowed." << endl;
     }
 }
-
+// adds extra days to the due date, default is 10
 void Book::extendDueDate(int extraDays) {
     if (isBorrowed) {
         dueDate += extraDays * 24 * 60 * 60;
